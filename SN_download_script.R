6673caa61b86e2d72088b60d54ac80e52a276502
mching <mikeching@yahoo.com>
2013-08-25 20:38:42 -1000

    Update SN_download_script.R
    add comment on how to change the download range

--- SN_download_script.R
+++ SN_download_script.R
@@ -1,5 +1,7 @@
 setwd("~/Documents/securitynow/shows")
 
+# change the i to go from the first show number you want to the last show number. As of 23 August 2013, the latest show is #418.
+
 for(i in 100:400) {
   shortname <- paste0("sn-", i, ".txt")
   showname <- paste0("https://www.grc.com/sn/", shortname)
