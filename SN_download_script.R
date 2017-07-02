7585f56cca436eb9fbaba2cea5101dd990c15257
Michael Ching <mikeching@yahoo.com>
2017-07-01 21:19:28 -1000

    update script to be a little more flexible with working directory specification

--- SN_download_script.R
+++ SN_download_script.R
@@ -1,10 +1,10 @@
-setwd("~/Documents/securitynow/shows")
+DIRECTORY <- "/Volumes/WD_6GB_Chings/media/Books/securitynow/shows"
 
-# change the i to go from the first show number you want to the last show number. As of 23 August 2013, the latest show is #418.
+# change the i to go from the first show number you want to the last show number. As of 1 July 2017, the most recent show is #618.
 
-for(i in 100:400) {
+for(i in 610:618) {
   shortname <- paste0("sn-", i, ".txt")
   showname <- paste0("https://www.grc.com/sn/", shortname)
   print(shortname)
-  download.file(showname, destfile = shortname, method = "curl")
+  download.file(showname, destfile = paste0(DIRECTORY, shortname), method = "curl")
 }
